b9ebf7ba2484d0ca5fc45569d419a136169637c4
Dalea Kattan <97688660+anask1234@users.noreply.github.com>
2022-05-01 05:04:48 +0000

    Still coding website, testing form data

---
+++ script.js
@@ -0,0 +1,36 @@
+function bruhtest(){
+  var month = document.getElementById('month');
+ var day = document.getElementById('day');
+ var year = document.getElementById('year');
+var countDownDate = new Date(month + day + year ,"12:00:01").getTime();
+
+    // Run myfunc every second
+    var myfunc = setInterval(function() {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("days").innerHTML = days + " Days remaining until the date selected!"
+    document.getElementById("hours").innerHTML = hours + "h " 
+    document.getElementById("mins").innerHTML = minutes + "m " 
+    document.getElementById("secs").innerHTML = seconds + "s " 
+        
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "TIMES UP!!";
+    }
+    }, 1000);
+   
+}
